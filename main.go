2d4522856cfa6fce6eff62c7790c33b97d77bb48
tobi <tobi19.py@gmail.com>
2025-05-15 17:42:19 -0500

    Print file data

--- main.go
+++ main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/tobib-dev/gator/internal/config"
 )
 
 func main() {
 	config.ReadJsonFile()
 	config.SetUser("Tobi")
+	file := config.ReadJsonFile()
+	fmt.Println(file.DbUrl)
+	fmt.Println(file.CurrentUserName)
 }
