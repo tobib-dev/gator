4764984bef15e9a4789085e06a22274df981ebaf
tobi <tobi19.py@gmail.com>
2025-05-19 14:57:25 -0500

    Implement delete users for easy testing

---
+++ handler_reset.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"context"
+	"fmt"
+)
+
+func handlerReset(s *state, cmd command) error {
+	err := s.db.DeleteUsers(context.Background())
+	if err != nil {
+		return fmt.Errorf("couldn't delete database: %w", err)
+	}
+	fmt.Println("Successfully reset database")
+
+	return nil
+}
